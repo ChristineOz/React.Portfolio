ac2cf3acdb0bbd4370654b736166b9de0b1bf6c7
Christine Osmak <christineosmak@gmail.com>
2021-07-14 20:28:36 -0400

    update

---
+++ src/resumeData.js
@@ -0,0 +1,93 @@
+let resumeData = {
+    "name": "Christine Osmak",
+    "role": "Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/christine-osmak-42808376/",
+    "skypeid": "Your skypeid",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/christine-osmak-42808376/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ChristineOz",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"email",
+          "url":"mailto: christineosmak@gmail.com",
+          "className":"fa fa-email"
+        }
+      ],
+    "aboutme":"An aspiring Front-End Web Developer with experience in HTML, CSS, Javascript, Bootstrap, React.js and Node.js.",
+    "address":"NJ & NYC",
+    "education":[
+      {
+        "UniversityName":"Rutgers University",
+        "specialization":"Full Stacked Web Developer",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2021",
+        "Achievements":"Certification"
+      },
+      {
+        "UniversityName":"Fashion Institute of Tehnology",
+        "specialization":"BS in Fashion Merchandising and Management",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2014",
+        "Achievements":"Graduate"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Commercial Seating Products",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Bet-Full Stack",
+        "description":"A web app that brings betting information on European Football (Premiere League) to an American audience",
+        "imgurl":"public/images/Bet-Full.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+  }
+  
+  export default resumeData
