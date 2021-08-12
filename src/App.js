4d2f3e9d7c6e33d6fdde27c9985da995e6cdc1de
Christine Osmak <christineosmak@gmail.com>
2021-08-12 18:22:17 -0400

    update

--- src/App.js
+++ src/App.js
@@ -18,13 +18,6 @@ class App extends Component {
         <Footer resumeData={resumeData}/>
       </div>
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-    );
   }
 }
 
