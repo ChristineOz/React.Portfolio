2eb7bb3d148857498489ac4dc6dc58195024e8a5
Christine Osmak <christineosmak@gmail.com>
2021-08-19 15:13:48 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,33 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-var CompressionPlugin = require('compression-webpack-plugin');
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-plugins: [
-    new webpack.DefinePlugin({ 
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new CompressionPlugin({   <-- Add this
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  ]
 
-  app.get('*.js', function (req, res, next) {
-    req.url = req.url + '.gz';
-    res.set('Content-Encoding', 'gzip');
-    next();
-  });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
