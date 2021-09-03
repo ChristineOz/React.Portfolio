3099d1d3ef91b9f0af8c67f7adac01b780563500
Christine Osmak <christineosmak@gmail.com>
2021-09-03 17:07:09 -0400

    update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,19 +15,19 @@ export default class Porfolio extends Component {
                 <div className="columns portfolio-item" >
                   <div className="item-wrap" >
                    
-                    
+                  <a href={item.link}>
                       <button>
                       <img src={`${item.imgurl}`} className="item-img" style={{width:'250px', height:'200px'}}/>
-                    <a href={item.link} style={{color:'white', fontWeight:'bolder', textAlign:'center', textShadow:'-1px -1px 0 #000,1px -1px 0 #000,-1px 1px 0 #000,	1px 1px 0 #000'}}>Explore</a></button>
+                    <a href={item.link} style={{color:'white', fontWeight:'bolder', textAlign:'center', textShadow:'-1px -1px 0 #000,1px -1px 0 #000,-1px 1px 0 #000,	1px 1px 0 #000'}}>Explore</a> </button>
                       <div className="overlay" style={{overflow:"scroll"}}>
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
                           <p>{item.description}</p>
-                         
+                          
                         </div>
                       </div>
                   
-                    
+                    </a>
                   </div>
                 </div>
               )
